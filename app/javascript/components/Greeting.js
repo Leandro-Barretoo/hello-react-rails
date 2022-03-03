fb6cb3a270bbef5e6daf03e17412080a7643516d
Leandro-Barretoo <officialdevmi@gmail.com>
2022-03-03 19:35:49 +0000

    Add react routes

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,10 +1,11 @@
 import React from "react"
 import PropTypes from "prop-types"
+
 class Greeting extends React.Component {
   render () {
     return (
       <React.Fragment>
-        Greeting: {this.props.greeting}
+        Greetings: {this.props.greetingcommand}
       </React.Fragment>
     );
   }
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root 'static#index'
   namespace :api, defaults: { format: 'json' } do
     get 'grettings', to: 'grettings#index'
   end
+
+  root 'static#index'
 end
